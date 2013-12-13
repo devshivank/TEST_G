8aa95e34f9595647e1e5af6571f28e2cd510c83d
shivank agarwal <shivank03mit@gmail.com>
2013-12-13 18:01:05 +0530

    Initial Commit

---
+++ TEST_G/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TEST_G
+//
+//  Created by Anurag Gupta on 13/12/13.
+//  Copyright (c) 2013 Shivank Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TEST_G/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TEST_G
+//
+//  Created by Anurag Gupta on 13/12/13.
+//  Copyright (c) 2013 Shivank Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
